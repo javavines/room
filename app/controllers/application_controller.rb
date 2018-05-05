a825eb3a6f522b3f392fddd8708d616b1738ba78
deepak raghuwanshi <tisen.raghuwans@gmeil.com>
2018-05-05 14:28:33 +0530

    first change on master after pagination commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
+
+class change_on_masteragter_agination
+	"1. master branch and after pagination"
+	"2. "
+	"3. "
+end
